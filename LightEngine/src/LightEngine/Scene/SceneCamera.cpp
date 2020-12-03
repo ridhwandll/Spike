6a6e79912e2931f0a8ca10d781be0dde162e550f
Fahim Fuad <fahimfuad.dev@gmail.com>
2020-12-03 16:48:14 +0600

    [Bug Fix] Fixed the camera bug, in which camera was working opposite!

--- LightEngine/src/LightEngine/Scene/SceneCamera.cpp
+++ LightEngine/src/LightEngine/Scene/SceneCamera.cpp
@@ -9,8 +9,18 @@ namespace LightEngine
         RecalculateProjection();
     }
 
+    void SceneCamera::SetPerspective(float verticalFOV, float nearClip, float farClip)
+    {
+        m_ProjectionType = ProjectionType::Perspective;
+        m_PerspectiveFOV = verticalFOV;
+        m_PerspectiveNear = nearClip;
+        m_PerspectiveFar = farClip;
+        RecalculateProjection();
+    }
+
     void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
     {
+        m_ProjectionType = ProjectionType::Orthographic;
         m_OrthographicSize = size;
         m_OrthographicNear = nearClip;
         m_OrthographicFar = farClip;
@@ -25,12 +35,21 @@ namespace LightEngine
 
     void SceneCamera::RecalculateProjection()
     {
-        float orthLeft = -m_OrthographicSize * m_AspectRatio * -0.5f;
-        float orthoRight = m_OrthographicSize * m_AspectRatio * -0.5f;
-        float orthoBottom = -m_OrthographicSize * -0.5f;
-        float orthoTop = m_OrthographicSize * -0.5f;
+        if (m_ProjectionType == ProjectionType::Perspective)
+        {
+            m_Projection = glm::perspective(m_PerspectiveFOV, m_AspectRatio, m_PerspectiveNear, m_PerspectiveFar);
+        }
+        else
+        {
+            float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
+            float orthoRight = m_OrthographicSize * m_AspectRatio * 0.5f;
+            float orthoBottom = -m_OrthographicSize * 0.5f;
+            float orthoTop = m_OrthographicSize * 0.5f;
 
-        m_Projection = glm::ortho(orthLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+            m_Projection = glm::ortho(orthoLeft, orthoRight,
+                orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+        }
+
     }
 
 }
